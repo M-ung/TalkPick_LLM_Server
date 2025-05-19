daa7099f6bd2583b737cfbdc0d69e316d2cadd5e
M-ung <wjdahrrla@gmail.com>
2025-05-19 11:06:29 +0900

    Refactor: DB 접근 코드 제거해라

--- app/common/config/settings.py
+++ app/common/config/settings.py
@@ -2,7 +2,6 @@ from pydantic_settings import BaseSettings
 from functools import lru_cache
 
 class Settings(BaseSettings):
-    # LLM Model settings
     MODEL_PATH: str
     MODEL_TEMPERATURE: float = 0.7
     MODEL_MAX_TOKENS: int = 2000
